8361df67a28720011458329018ee0a9e44f100bd
moslemajra85 <144964665+moslemajra85@users.noreply.github.com>
2024-11-02 17:21:18 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,60 @@
+const mongoose = require('mongoose');
+
+mongoose
+  .connect('mongodb://localhost/playground')
+  .then(() => console.log('Connected to mongodDb...'))
+  .catch((error) => console.error('We could not connect to mongodb: ', error));
+
+const authorSchema = new mongoose.Schema({
+  name: String,
+  bio: String,
+  website: String,
+});
+
+const Author = mongoose.model('Author', authorSchema);
+
+const courseSchema = new mongoose.Schema({
+  name: String,
+});
+
+const Course = mongoose.model('Course', courseSchema);
+
+async function createAuthor(name, bio, website) {
+  try {
+    const author = new Author({
+      name,
+      bio,
+      website,
+    });
+
+    const result = await author.save();
+    console.log(result);
+  } catch (error) {
+    console.log(error);
+  }
+}
+async function createCourse(name, author) {
+  try {
+    const course = new Course({
+      name,
+      author,
+    });
+
+    const result = await course.save();
+    console.log(result);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function listCourses() {
+  try {
+    const courses = await Course.find().select('name -_id');
+    console.log(courses);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+//createCourse('Node.js', createAuthor());
+listCourses()
